04360107b7569c2efa6a503d839857b00a04dd20
kmtym1998 <kmtym.devloper@gmail.com>
2023-07-29 19:40:14 +0900

    audio file node

---
+++ node/audio_file.go
@@ -0,0 +1,34 @@
+package node
+
+import (
+	"crypto/md5"
+	"fmt"
+
+	"github.com/kmtym1998/es-indexer/elasticsearch"
+	"github.com/samber/lo"
+)
+
+type AudioFile struct {
+	FilePath string   `json:"filePath"`
+	FileName string   `json:"fileName"`
+	Artist   string   `json:"artist"`
+	Album    string   `json:"album"`
+	Title    string   `json:"title"`
+	Tags     []string `json:"tags"`
+}
+
+type AudioFileList []AudioFile
+
+func (a AudioFile) NodeIdentifier() string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(a.FilePath)))
+}
+
+func (a AudioFileList) IndexName() string {
+	return "audio_files"
+}
+
+func (a AudioFileList) ToList() []elasticsearch.DocumentNode {
+	return lo.Map(a, func(item AudioFile, _ int) elasticsearch.DocumentNode {
+		return item
+	})
+}
